7ce0bf27c28c57540a14873ada3b2e3b3c003162
Cedric Arts <152818750+cedricarts@users.noreply.github.com>
2024-09-04 21:56:34 +0200

    Update psg.py
    Made changes so that user can now choose whether they want a password or not

--- psg.py
+++ psg.py
@@ -1,18 +1,23 @@
 #Password Generator
 import random
+ answer = input("Do you want a password?:" )
+def GeneratePassword():
+    if answer == 'yes':
+        # Allowing user to change length of password
+        length = input("How long should the password be: ")
 
-# Allowing user to change length of password
-length = input("How long should the password be: ")
+        # Declaring list of characters, digits to be used in password
+        pass1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-# Declaring list of characters, digits to be used in password
-pass1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        password = ""
 
-password = ""
+        # Generating password
+        for x in range(int(length)):
+            password = password + random.choices(pass1)[0]
+    else:
+        print("Goodbye, have a great day!!")
 
-# Generating password
-for x in range(int(length)):
-    password = password + random.choice(pass1)[0]
-
+GeneratePassword()
 #Displaying password to user
 print("Your new password is\n: ", password)
     
