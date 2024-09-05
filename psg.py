d8c3ff18322e91242e3fab171d2e2305fd38a6a5
Cedric Arts <152818750+cedricarts@users.noreply.github.com>
2024-09-05 22:34:27 +0200

    Update psg.py
    Now it can allow user to select what the password to consist of, and prompts user to confirm if they want to generate a password or not

--- psg.py
+++ psg.py
@@ -1,26 +1,38 @@
 #Password Generator
 import random
-import tkinter
-import sys
 
+generate = input("Hello, do you wish to generate a password? ")
 
 def GeneratePassword():
-  answer = input("Do you want a password?:" )
-    if answer == 'yes':
+
+    contains = input("What do you want your password to consist of (digits/characters/symbols)?: ")
+
         # Allowing user to change length of password
-        length = input("How long should the password be: ")
+    length = input("How long should the password be: ")
 
         # Declaring list of characters, digits to be used in password
-        pass1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    if contains.lower() == "digits":
+            pass1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    if contains.lower() == "characters":
+            pass1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                     'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D']
+    if contains.lower() == "symbols":
+           pass1 = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+    if contains.lower() == "":
+          print("Please enter a data type!")
 
-        password = ""
+    password = ""
 
         # Generating password
-        for x in range(int(length)):
-            password = password + random.choices(pass1)[0]
-    else:
-        print("Goodbye, have a great day!!")
-#Displaying password to user
-print("Your new password is\n: ", password)
-GeneratePassword()
-    
+    for x in range(int(length)):
+         password = password + random.choice(pass1)[0]
+
+        #Displaying password to user
+    print("Your new password is\n: ", password)
+
+
+if generate.lower() == 'yes':
+    GeneratePassword()
+else:
+    print("Have a wonderful and blessed day!!")
